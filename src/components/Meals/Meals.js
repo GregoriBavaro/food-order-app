0cd2c0775216f501acd7af5dc2477e6b3925ece5
Gregori <greg.gego@gmail.com>
2023-01-12 22:07:46 +0100

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import MealsSummery from "./MealsSummery";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummery />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
